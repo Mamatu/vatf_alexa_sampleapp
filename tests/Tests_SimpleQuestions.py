899d6da3e0f37ec2d710308a9434ee76bb1c01fd
Marcin Matula <marcin.matula@gmail.com>
2022-07-05 22:32:23 +0200

    update

--- tests/Tests_SimpleQuestions.py
+++ tests/Tests_SimpleQuestions.py
@@ -2,13 +2,13 @@ from vatf import vatf_init
 from vatf.generator import gen_tests
 from vatf.api import audio, player, wait, shell
 
-def Test_One_Question():
+def test_one_question():
     audio.record_inputs_outputs()
     shell.fg("echo 't' > /tmp/alexa_input.pipe")
     player.play_audio("alexa_are_you_there.wav")
     wait.sleep(5)
 
-def Test_Two_Questions():
+def test_two_questions():
     audio.record_inputs_outputs()
     shell.fg("echo 't' > /tmp/alexa_input.pipe")
     player.play_audio("alexa_are_you_there.wav")
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     import sys
     import logging
     logging.basicConfig(level=logging.DEBUG)
-    gen_tests.create_test(sys.argv[1], Test_One_Question.__name__, Test_One_Question)
-    gen_tests.create_test(sys.argv[1], Test_Two_Questions.__name__, Test_Two_Questions)
+    gen_tests.create_test(sys.argv[1], test_one_question.__name__, test_one_question)
+    gen_tests.create_test(sys.argv[1], test_two_questions.__name__, test_two_questions)
