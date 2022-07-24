66161c0e2a1cd8f74057921fd80e836c505cabe0
Marcin Matula <marcin.matula@gmail.com>
2022-07-24 22:51:33 +0200

    tests update

--- tests/Tests_SimpleQuestions.py
+++ tests/Tests_SimpleQuestions.py
@@ -12,7 +12,7 @@ def test_two_questions():
     audio.record_inputs_outputs()
     shell.fg("echo 't' > /tmp/alexa_input.pipe")
     player.play_audio("alexa_are_you_there.wav")
-    wait.wait_for_regex(regex = ".*DialogUXStateAggregator:executeSetState:from=SPEAKING,to=IDLE,validTransition=true.*", log_path = None, timeout = 600, pause = 0.5)
+    wait.wait_for_regex(regex = ".*DialogUXStateAggregator:executeSetState:from=SPEAKING,to=IDLE,validTransition=true.*", timeout = 600, pause = 0.5)
     shell.fg("echo 't' > /tmp/alexa_input.pipe")
     player.play_audio("alexa_tell_me_a_joke.wav")
 
