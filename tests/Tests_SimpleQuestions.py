7079b5cb15c2fb20c40fdad1d753231ca4a55415
Marcin Matula <marcin.matula@gmail.com>
2022-12-18 23:17:06 +0100

    Tests_SimpleQuestion.py - modification

--- tests/Tests_SimpleQuestions.py
+++ tests/Tests_SimpleQuestions.py
@@ -5,15 +5,16 @@ from vatf.utils import config_handler
 
 def setup_function():
     config_handler.init_configs(["./config.json"])
+    #config_handler.init_configs(["./config.py"])
 
-def test_one_question():
-    audio.record_inputs_outputs()
+def test_example_1():
+    audio.record_inputs_outputs_from_config()
     shell.fg("echo 't' > /tmp/alexa_input.pipe")
     player.play_audio("alexa_are_you_there.wav")
     wait.sleep(5)
 
-def test_two_questions():
-    audio.record_inputs_outputs()
+def test_example_2():
+    audio.record_inputs_outputs_from_config()
     shell.fg("echo 't' > /tmp/alexa_input.pipe")
     player.play_audio("alexa_are_you_there.wav")
     wait.wait_for_regex(regex = ".*DialogUXStateAggregator:executeSetState:from=SPEAKING,to=IDLE,validTransition=true.*", timeout = 600, pause = 0.5)
