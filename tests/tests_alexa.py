ee8dbce414c088fe55955c0cc8c8feb32055b435
Marcin Matula <marcin.matula@gmail.com>
2023-01-08 23:04:55 +0100

    tests_alexa.py - new implementation

--- tests/tests_alexa.py
+++ tests/tests_alexa.py
@@ -2,6 +2,7 @@ from vatf import vatf_init
 from vatf.generator import gen_tests
 from vatf.api import audio, player, wait, shell, log_snapshot
 from vatf.utils import config_handler
+from vatf.utils import wait_types as w_t
 
 def setup_function():
     config_handler.init_configs(["./config.json"])
@@ -13,14 +14,34 @@ def teardown_function():
     audio.stop()
     log_snapshot.stop()
 
-def test_1():
+def regex_alexa_from_state_to_state(state_1, state_2):
+    return f"DialogUXStateAggregator:executeSetState:from={state_1},to={state_2},validTransition=true"
+
+def regex_speaking_to_idle():
+    return regex_alexa_from_state_to_state("SPEAKING", "IDLE")
+
+def regex_listening_to_idle():
+    return regex_alexa_from_state_to_state("LISTENING", "IDLE")
+
+def play_audio(utt):
     shell.fg("echo 't' > /tmp/alexa_input.pipe")
-    player.play_audio("alexa_are_you_there.wav")
-    wait.wait_for_regex(regex = ".*DialogUXStateAggregator:executeSetState:from=SPEAKING,to=IDLE,validTransition=true.*", timeout = 600, pause = 0.5)
+    player.play_audio(utt)
+
+def test_1():
+    play_audio("alexa_are_you_there.wav")
+    wait.wait_for_regex(regex = regex_speaking_to_idle(), timeout = 10, pause = 0.5)
 
 def test_2():
-    shell.fg("echo 't' > /tmp/alexa_input.pipe")
-    player.play_audio("alexa_are_you_there.wav")
-    wait.wait_for_regex(regex = ".*DialogUXStateAggregator:executeSetState:from=SPEAKING,to=IDLE,validTransition=true.*", timeout = 600, pause = 0.5)
-    shell.fg("echo 't' > /tmp/alexa_input.pipe")
-    player.play_audio("alexa_tell_me_a_joke.wav")
+    play_audio("alexa_dasfas.wav")
+    labels = {}
+    assert wait.wait_for_regex(regex = [[regex_speaking_to_idle(), w_t.Label("s_t_i")], [regex_listening_to_idle(), w_t.Label("l_t_i")], w_t.RegexOperator.OR], timeout = 10, pause = 0.5, labels = labels)
+    assert labels["l_t_i"]
+    assert not labels["s_t_i"]
+    if labels["l_t_i"]:
+        play_audio("alexa_are_you_there.wav")
+        labels = {}
+        assert wait.wait_for_regex(regex = [[regex_speaking_to_idle(), w_t.Label("s_t_i")], [regex_listening_to_idle(), w_t.Label("l_t_i")], w_t.RegexOperator.OR], timeout = 10, pause = 0.5, labels = labels)
+        assert labels["s_t_i"]
+        assert not labels["l_t_i"]
+    play_audio("alexa_tell_me_a_joke.wav")
+    assert wait.wait_for_regex(regex = regex_speaking_to_idle(), timeout = 10, pause = 0.5)
